8467e287116464f7635a95bdca5ae1813c99b06e
letrananhvu <vule5726@gmail.com>
2019-04-03 09:44:12 +0700

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(serveStatic(path.join(__dirname + '/dist')));
+
 app.listen(process.env.PORT || 4000, ()=>{
   console.log('server is openning');
 });
